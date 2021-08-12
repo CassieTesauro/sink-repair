66312a7e451c438e019177c7e647abb2b62cffba
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-12 14:17:05 -0500

    added signal flow notes

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,16 +1,18 @@
-const applicationState = { 
+//Since 
+const applicationState = {
     requests: []  //B5CH4 "You will need to store that external data in your application state when you fetch it. Create a property named requests in your application state object."
 }
 
-const API = "http://localhost:8088"
+const API = "http://localhost:8088" //7. see that API references a URL (url of where the API is online)
 
-export const fetchRequests = () => {
-    return fetch(`${API}/requests`)
-        .then(response => response.json())
+export const fetchRequests = () => { //5. define function fetchRequests. purpose: get JSON data from API, turn it into JS
+    return fetch(`${API}/requests`) //6. take ${API}/requests and pass it to fetch.  Need to find what API references.
+                                    //8.  now know that we are fetching the requests data section of the API'S website
+        .then(response => response.json())  //9. taking the response of the promise{} made in step 6.  Right now the response is an encoded JSON string. So we use .json() on the response to parse it into JS data.   
         .then(
-            (serviceRequests) => {
+            (serviceRequests) => { //10. Take the JS data from step 9 and pass it into this .then().  now the JS data is referenced by 'serviceRequests'. AKA serviceRequests references the external state we fetched from the API
                 // Store the external state in application state
-                applicationState.requests = serviceRequests
+                applicationState.requests = serviceRequests  //11. now that servic
             }
         )
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,22 @@
-import { SinkRepair } from "./SinkRepair.js"
+import { fetchRequests } from "./dataAccess.js"  //dataAccess has the application state aobject that holds the requests array.  that's where we are storing the data we fetched from the API.
+import { SinkRepair } from "./SinkRepair.js"  //has the sinkRepair() definition
 
 
+//allow dynamic html written in js files and render it inside html #comtainer
 const mainContainer = document.querySelector("#container")
 
+//since we're fetching api data to be used in the rendered html, the render function definition needs to run fetchRequests, then call sink repair to populate the main container's inner html.
 const render = () => {
-    mainContainer.innerHTML = SinkRepair()
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
 }
 
-render()
+render()  //call the render function.  This will make our html render on the screen for the user.
+
+
+//
+
 
